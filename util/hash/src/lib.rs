cab0f79b4503ad85903489f36a0e3b94070eb1dd
Your Name <you@example.com>
2019-05-25 22:42:43 +0000

    fix dual progpow caches

--- util/hash/src/lib.rs
+++ util/hash/src/lib.rs
@@ -35,7 +35,7 @@ extern {
 	pub fn keccak_256(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) -> i32;
 	/// Hashes input. Returns -1 if either out or input does not exist. Otherwise returns 0.
 	pub fn keccak_512(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) -> i32;
-    pub fn get_block_progpow_hash(header: *const [u8; 32],
+    pub fn get_block_progpow_hash(epoch: u32, header: *const [u8; 32],
                                   nonce: u64, out: *mut [u8; 64]) -> i32;
 	pub fn get_from_mix(header: *const [u8; 32], nonce: u64,
 	  mix: *const [u8;32], out: *mut [u8; 32]) -> i32;
